b3c86880a7fb3c7aa22a401ae183a53078659ce8
Jack Linhart <iatenine@gmail.com>
2021-08-18 23:12:06 -0600

    Imported local module

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
-// TODO: Include packages needed for this application
+const nosyReporter = require("inquirer");
+const fileSystem = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  "Project name: ",
+  "Enter project description: ",
+  "Enter installation instructions: ",
+  "Describe the project's usage: ",
+  "How can one contribute to this project? ",
+  "Specify any tests (leave blank if none): ",
+  "Frequently asked Questions: ",
+];
+const varNames = [
+  "projectName",
+  "description",
+  "installation",
+  "usage",
+  "contributing",
+  "tests",
+  "faq",
+];
+
+function getQuestionArray() {
+  const ret = [];
+  questions.map((elem, index) => {
+    const frag = {
+      message: elem,
+      name: varNames[index],
+    };
+    if (index == varNames.length - 1) {
+      frag["type"] = "list";
+      frag["choices"] = [3, 4, 6];
+    } else frag["type"] = "input";
+    ret.push(frag);
+  });
+
+  return ret;
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  nosyReporter
+    .prompt(getQuestionArray())
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  getQuestionArray();
+}
 
 // Function call to initialize app
 init();
